1c7e543dc03cca62bbe6a1bd535947864dce9288
daisy-oss <77732121+daisy-oss@users.noreply.github.com>
2021-05-21 08:44:35 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,109 @@
+var canvas=new fabric.Canvas("myCanvas");
+player_x=10;
+player_y=10;
+block_image_width=30;
+block_image_height=30;
+var player_object="";
+var block_image_object="";
+
+function player_update(){
+    fabric.Image.fromURL(" player.png",function(Img){
+        player_object=Img;
+        player_object.scaleToWidth(150);
+        player_object.scaleToHeight(140);
+        player_object.set({
+            top:player_y,left:player_x
+        });
+        canvas.add(player_object);
+    });
+}
+
+function new_image(get_image){
+    fabric.Image.fromURL(get_image,function(Img){
+        block_image_object=Img;
+        block_image_object.scaleToWidth(block_image_width);
+        block_image_object.scaleToHeight(block_image_height);
+        block_image_object.set({
+            top:player_y,left:player_x
+        });
+        canvas.add(block_image_object);
+    });
+}
+window.addEventListener("keydown",my_keydown);
+function my_keydown(e){
+keyPressed=e.keyCode;
+if(e.shiftKey==true && keyPressed=='80'){
+    block_image_width=block_image_width+10;
+    block_image_height=block_image_height+10;
+    document.getElementById("current_width").innerHTML=block_image_width;
+    document.getElementById("current_height").innerHTML=block_image_height;
+}
+if(e.shiftKey==true && keyPressed=='77'){
+    block_image_width=block_image_width-10;
+    block_image_height=block_image_height-10;
+    document.getElementById("current_width").innerHTML=block_image_width;
+    document.getElementById("current_height").innerHTML=block_image_height;
+}
+
+if(keyPressed=='70'){
+    new_image("ironman_face.png");
+    console.log("F");
+}
+if(keyPressed=='82'){
+    new_image("thor_right_hand.png");
+    console.log("R");
+}
+if(keyPressed=='72'){
+    new_image("captain_america_left_hand.png");
+    console.log("H");
+}
+if(keyPressed=='76'){
+    new_image("hulk_legs.png");
+    console.log("L");
+}    
+if(keyPressed=='66'){
+    new_image("spiderman_body.png");
+    console.log("B");            
+}
+if(keyPressed=='37'){
+    left();
+}
+if(keyPressed=='38'){
+    up();
+}
+if(keyPressed=='39'){
+    right();
+}
+if(keyPressed=='40'){
+    down();
+}
+}
+
+function up(){
+    if(player_y>=0){
+        player_y=player_y-block_image_height;
+        canvas.remove(player_object);
+        player_update();
+    }
+}
+function down(){
+    if(player_y<=500){
+        player_y=player_y+block_image_height;
+        canvas.remove(player_object);
+        player_update();
+    }
+}
+function left(){
+    if(player_x>=0){
+        player_x=player_x-block_image_width;
+        canvas.remove(player_object);
+        player_update();
+    }
+}
+function right(){
+    if(player_x<=850){
+        player_x=player_x+block_image_width;
+        canvas.remove(player_object);
+        player_update();
+    }
+}
